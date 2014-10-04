c2ffeb6edfdff2b861d62b77bcdbca7333d03727
OCHIAI, Gouji <gjo.ext@gmail.com>
2014-10-04 15:02:51 +0900

    add tests for string

--- colander_jsonschema/tests/test_colander_jsonschema.py
+++ colander_jsonschema/tests/test_colander_jsonschema.py
@@ -1,12 +1,166 @@
 # -*- coding: utf-8 -*-
 
 import unittest
-import colander
+
+
+class StringNodeTestCase(unittest.TestCase):
+
+    def test_minimal(self):
+        import colander
+        from .. import convert
+        node = colander.SchemaNode(colander.String())
+        ret = convert(node)
+        self.assertDictEqual(ret, {
+            '$schema': 'http://json-schema.org/draft-04/schema#',
+            'type': 'string',
+            'minLength': 1,
+        })
+
+    def test_can_null(self):
+        import colander
+        from .. import convert
+        node = colander.SchemaNode(colander.String(), missing=None)
+        ret = convert(node)
+        self.assertDictEqual(ret, {
+            '$schema': 'http://json-schema.org/draft-04/schema#',
+            'type': ['string', 'null'],
+        })
+
+    def test_has_default(self):
+        import colander
+        from .. import convert
+        node = colander.SchemaNode(colander.String(), default='DEFAULT')
+        ret = convert(node)
+        self.assertDictEqual(ret, {
+            '$schema': 'http://json-schema.org/draft-04/schema#',
+            'type': 'string',
+            'minLength': 1,
+            'default': 'DEFAULT',
+        })
+
+    def test_can_null_has_default(self):
+        import colander
+        from .. import convert
+        node = colander.SchemaNode(colander.String(), missing='DEFAULT',
+                                   default='DEFAULT')
+        ret = convert(node)
+        self.assertDictEqual(ret, {
+            '$schema': 'http://json-schema.org/draft-04/schema#',
+            'type': ['string', 'null'],
+            'default': 'DEFAULT',
+        })
+
+    def test_validate_length_both(self):
+        import colander
+        from .. import convert
+        node = colander.SchemaNode(colander.String(),
+                                   validator=colander.Length(11, 33))
+        ret = convert(node)
+        self.assertDictEqual(ret, {
+            '$schema': 'http://json-schema.org/draft-04/schema#',
+            'type': 'string',
+            'maxLength': 33,
+            'minLength': 11,
+        })
+
+    def test_validate_length_min_and_null(self):
+        import colander
+        from .. import convert
+        node = colander.SchemaNode(colander.String(),
+                                   validator=colander.Length(22),
+                                   missing=None)
+        ret = convert(node)
+        self.assertDictEqual(ret, {
+            '$schema': 'http://json-schema.org/draft-04/schema#',
+            'type': ['string', 'null'],
+            'minLength': 22,
+        })
+
+    def test_validate_length_max(self):
+        import colander
+        from .. import convert
+        node = colander.SchemaNode(colander.String(),
+                                   validator=colander.Length(max=44),
+                                   missing=None)
+        ret = convert(node)
+        self.assertDictEqual(ret, {
+            '$schema': 'http://json-schema.org/draft-04/schema#',
+            'type': ['string', 'null'],
+            'maxLength': 44,
+        })
+
+    def test_validate_length_max_and_null(self):
+        import colander
+        from .. import convert
+        node = colander.SchemaNode(colander.String(),
+                                   validator=colander.Length(max=55))
+        ret = convert(node)
+        self.assertDictEqual(ret, {
+            '$schema': 'http://json-schema.org/draft-04/schema#',
+            'type': 'string',
+            'maxLength': 55,
+            'minLength': 1,
+        })
+
+    def test_validate_regex(self):
+        import colander
+        from .. import convert
+        node = colander.SchemaNode(colander.String(),
+                                   validator=colander.Regex(ur'TESTtestTEST'))
+        ret = convert(node)
+        self.assertDictEqual(ret, {
+            '$schema': 'http://json-schema.org/draft-04/schema#',
+            'type': 'string',
+            'pattern': 'TESTtestTEST',
+            'minLength': 1,
+        })
+
+    def test_validate_regex_email(self):
+        import colander
+        from .. import convert
+        node = colander.SchemaNode(colander.String(),
+                                   validator=colander.Email())
+        ret = convert(node)
+        self.assertDictEqual(ret, {
+            '$schema': 'http://json-schema.org/draft-04/schema#',
+            'type': 'string',
+            'format': 'email',
+            'minLength': 1,
+        })
+
+    def test_validate_oneof(self):
+        import colander
+        from .. import convert
+        node = colander.SchemaNode(colander.String(),
+                                   validator=colander.OneOf(["one", "two"]))
+        ret = convert(node)
+        self.assertDictEqual(ret, {
+            '$schema': 'http://json-schema.org/draft-04/schema#',
+            'type': 'string',
+            'enum': ['one', 'two'],
+            'minLength': 1,
+        })
+
+    def test_validate_oneof_null(self):
+        import colander
+        from .. import convert
+        node = colander.SchemaNode(colander.String(),
+                                   validator=colander.OneOf(["one", "two"]),
+                                   missing=None)
+        ret = convert(node)
+        self.assertDictEqual(ret, {
+            '$schema': 'http://json-schema.org/draft-04/schema#',
+            'type': ['string', 'null'],
+            'enum': ['one', 'two', '', None],
+        })
+
+    dummy = None
 
 
 class ConvertTestCase(unittest.TestCase):
 
-    def test_mapping(self):
+    def tes_mapping(self):
+        import colander
 
         class CheckMapping(colander.MappingSchema):
             var_b = colander.SchemaNode(colander.Bool())
