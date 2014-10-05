ac4b75517ad656e697fabc73e74f56ab9fba3373
OCHIAI, Gouji <gjo.ext@gmail.com>
2014-10-06 00:49:49 +0900

    bump version

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ except:
 
 setup(
     name='colander_jsonschema',
-    version='0.2dev',
+    version='0.2',
     description='JSON-Schema converter for colander',
     long_description=long_description,
     author='OCHIAI, Gouji',
